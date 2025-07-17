7fed3e7f13eb2d3d7a6d4106d30d1c5783f6862b
iqonlimited <pritibalyan501@gmail.com>
2025-07-17 17:33:27 +0530

    login_popup.js

--- static/js/login_popup.js
+++ static/js/login_popup.js
@@ -1,11 +1,17 @@
+// Show the popup on page load
 window.onload = function() {
-  document.getElementById("loginPopup").style.display = "flex";
+  document.getElementById('loginPopup').style.display = 'flex';
 };
 
+// Login function
 function login() {
-  window.location.href = "/login";
+  document.getElementById('loginPopup').style.display = 'none';
+  // Redirect to login page
+  window.location.href = '/login';
 }
 
+// Guest Access function
 function guestAccess() {
-  document.getElementById("loginPopup").style.display = "none";
+  document.getElementById('loginPopup').style.display = 'none';
+  // Continue using the app as guest
 }
