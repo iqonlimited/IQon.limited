a19ddae9fcf6d20fdf93467ead62215b583a545d
iqonlimited <pritibalyan501@gmail.com>
2025-07-18 12:34:13 +0530

    toast.js

---
+++ static/js/toast.js
@@ -0,0 +1,7 @@
+function showToast(message) {
+  let toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.innerText = message;
+  document.body.appendChild(toast);
+  setTimeout(() => { toast.remove(); }, 3000);
+}
